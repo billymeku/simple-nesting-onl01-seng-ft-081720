0bf3b4f7bb209ba5b923024a6f09709275347d1e
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-24 08:05:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,9 +83,9 @@ def adding_matz
         }
      }
  
- programmer_hash[:yukihiro_matsumoto][:known_for] << "ruby"
+ programmer_hash[:yukihiro_matsumoto][:known_for] << "ruby"[:languages] <<["LISP", "C"]
  
-  programmer_hash[:yukihiro_matsumoto][:known_for][:languages] <<["LISP", "C"]
+#programmer_hash[:yukihiro_matsumoto][:known_for][:languages] <<["LISP", "C"]
 end
 
 def changing_alan
