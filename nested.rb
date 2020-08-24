529fb222450dd692b12a1b32b1f19987d99ea411
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-24 07:50:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,8 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     languages =programmer_hash[dennis_ritchie][:languages]
-     [languages[0]]
+     languages = programmer_hash[:dennis_ritchie][:languages]
+     languages[0]
 end
 
 def adding_matz
